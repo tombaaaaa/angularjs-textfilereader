e85b8261ba9c1d363217008dbf336246397531e8
Tom Brown <tombaaaaa@gmail.com>
2013-02-07 14:16:33 +0000

    Updated directive and README

--- README.md
+++ README.md
@@ -3,6 +3,9 @@ angularjs-textfilereader
 
 Directives for reading text files using the HTML5 FileReader
 
-Use as follows in your template:
+Pull requests welcome!
 
-&lt;filereader&gt;&lt;/filereader&gt;
+Use as follows:
+
+&lt;filereader files=&quot;myFiles&quot;&gt;&lt;/filereader&gt;
+&lt;pre&gt;{{myFiles | json}}&lt;/pre&gt;
--- directives.js
+++ directives.js
@@ -6,36 +6,36 @@ var myDirectives = angular.module('myApp.directives', []);
 
 myDirectives.directive('filereader', function ($compile) {
   return {
-    scope: {},
-    controller: function($scope) {
-      $scope.files = [];
-
-      $scope.upload = function (element) {
-        $scope.$apply(function ($scope) {
-          $scope.files = element.files;
+    scope: {
+      files: '='
+    },
+    restrict: 'E',
+    template: "<input type='file' onchange='angular.element(this).scope().upload(this)'>",
+    link: function (scope, element, attrs) {
+      scope.upload = function (element) {
+        scope.$apply(function (scope) {
+          scope.files = element.files;
         });
       };
 
-      $scope.$watch('files', function () {
+      scope.$watch('files', function () {
+        if (scope.files) {
+          for (var i = 0; i < scope.files.length; i += 1) {
+            var current = scope.files[i];
+            var reader = new FileReader();
 
-        for (var i = 0; i < $scope.files.length; i += 1) {
-          var current = $scope.files[i];
-          var reader = new FileReader();
+            reader.onload = (function (file) {
+              return function (env) {
+                scope.$apply(function () {
+                  current.contents = env.target.result;
+              });
+              }
+            }(current));
 
-          reader.onload = (function (file) {
-            return function (env) {
-              $scope.$apply(function () {
-                current.contents = env.target.result;
-            });
-            }
-          }(current));
-
-          reader.readAsText(current);
+            reader.readAsText(current);
+          }
         }
-
       }, true);
-    },
-    restrict: 'E',
-    template: "<pre>{{files | json}}</pre><input type='file' onchange='angular.element(this).scope().upload(this)'>",
+    }
   };
 });
