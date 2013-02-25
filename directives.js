4d6dc16ff502ad07df4390c6b3ff5f46c645a7c5
Tom Brown <tombaaaaa@gmail.com>
2013-02-25 17:49:44 +0000

    Simplified filereader directive

--- directives.js
+++ directives.js
@@ -7,33 +7,28 @@ var myDirectives = angular.module('myApp.directives', []);
 myDirectives.directive('filereader', function ($compile) {
   return {
     scope: {
-      files: '='
+      file: '='
     },
     restrict: 'E',
     template: "<input type='file' onchange='angular.element(this).scope().upload(this)'>",
     link: function (scope, element, attrs) {
       scope.upload = function (element) {
         scope.$apply(function (scope) {
-          scope.files = element.files;
+          scope.file = element.files[0];
         });
       };
 
-      scope.$watch('files', function () {
-        if (scope.files) {
-          for (var i = 0; i < scope.files.length; i += 1) {
-            var current = scope.files[i];
-            var reader = new FileReader();
-
-            reader.onload = (function (file) {
-              return function (env) {
-                scope.$apply(function () {
-                  current.contents = env.target.result;
+      scope.$watch('file', function () {
+        if (scope.file) {
+          var reader = new FileReader();
+          reader.onload = (function (file) {
+            return function (env) {
+              scope.$apply(function () {
+                scope.file.contents = env.target.result;
               });
-              }
-            }(current));
-
-            reader.readAsText(current);
-          }
+            }
+          }(scope.file));
+          reader.readAsText(scope.file);
         }
       }, true);
     }
